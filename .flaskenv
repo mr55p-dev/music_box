04094448c08b6cf40f7a35c7b3cfd964e146e0a3
Ellis <ellislunnon@mail.com>
2020-06-18 00:20:29 +0100

    Changes to templating and starting broswer audio refactoring.

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=2000
+export FLASK_RUN_PORT=80
--- app.py
+++ app.py
@@ -4,25 +4,39 @@ from multiprocessing import Process
 from dotenv import load_dotenv
 
 load_dotenv()
-app=Flask(__name__)
+app = Flask(__name__)
+
+
+cardTitle = ([],)
+cardBody = []
+cards = [
+    [
+        "Lizst - Hungarian Rhapsody no. 12",
+        "Music box style excerpt from our piece.",
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ien.com%2Ffiles%2Fbase%2Findm%2Fien%2Fimage%2F2018%2F05%2F640w%2Fpiano_keys.5aec75c0e81fa.jpg&f=1&nofb=1",
+    ],
+]
+
 
 def playTheMusic():
-    subprocess.run(['ffplay', '-nodisp', '-autoexit','./audio/file.mp3'])
+    subprocess.run(["ffplay", "-nodisp", "-autoexit", "./static/audio/file.mp3"])
     # output.check_returncode()
     # return output.returncode
     return
 
-@app.route('/')
+
+@app.route("/")
 def rootPage():
-    return(render_template('index.html', cardTitle=["Lizst - Hungarian Rhapsody no. 12"], cardBody=["Music box style excerpt from our piece."]))
+    return render_template("index.html", pageTitle="Music Box Home", cards=cards)
     # return "This is a page"
 
 
-@app.route('/play/')
+@app.route("/play/")
 def play():
     playTheMusic()
-    return redirect(url_for('rootPage'))
+    return redirect(url_for("rootPage"))
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port='80', debug=True)
+    app.run(host="0.0.0.0", port="80", debug=True)
     # app.run()
