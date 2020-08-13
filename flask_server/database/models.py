6d6276e6e2b87ba53361c61a616e1d4ed4a1b142
Ellis <ellislunnon@gmail.com>
2020-08-13 14:26:20 +0100

    Added some logic to control different music boxes and register a new box

--- flask_server/database/models.py
+++ flask_server/database/models.py
@@ -8,6 +8,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique = True)
     name = db.Column(db.String(100))
     password = db.Column(db.String(1000))
+    boxes = db.relationship('Box', backref=db.backref('owner'), lazy=False)
 
     def __repr__(self):
         return f"<User {self.name}>"
+
+class Box(db.Model):
+    __tablename__ = "boxes"
+
+    boxID = db.Column(db.Integer, primary_key=True)
+    boxName = db.Column(db.String(100), unique=True)
+    boxToken = db.Column(db.String(100))
+    userID = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"<Box {self.boxName}"
--- flask_server/routes/main.py
+++ flask_server/routes/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request, url_for, redirect
 from flask_login import current_user, login_required
+from flask_server.database.models import User, Box
 from flask_server import db
 
 main = Blueprint('main', __name__)
@@ -11,8 +12,44 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    userBoxes = Box.query.filter_by(userID=current_user.id).all()
+    return render_template('profile.html', 
+        name = current_user.name,
+        boxes = userBoxes
+    )
 
-@main.route('/create')
-def createDB():
-    pass
+@main.route('/register', methods=["POST"])
+@login_required
+def registerBox():
+    boxName = request.form.get("boxName")
+    boxToken = request.form.get("boxToken")
+
+    if Box.query.filter_by(boxToken=boxToken).first():
+        flash("This box token is already registered.")
+        return redirect(url_for('main.register'))
+    
+    newBox = Box(
+        boxName = boxName,
+        boxToken = boxToken,
+        userID = current_user.id
+    )
+    db.session.add(newBox)
+    db.session.commit()
+    flash("Your box has been added.")
+    return redirect(url_for('main.profile'))
+
+@main.route('/box/<boxID>')
+@login_required
+def boxView(boxID):
+    # Validate if the current user is the registered user of the box
+    # if current_user.id not in [i.userID for i in Box.query.filter_by(boxID=boxID).all()]:
+    #     flash("This box is not registered to you, please check the link and try again.")
+    #     return redirect(url_for('main.profile'))
+
+    current_box = Box.query.filter_by(boxID=boxID).first()
+    if current_box.userID is not current_user.id:
+        flash("This box is not registered to you, please check the link and try again.")
+        return redirect(url_for('main.profile'))
+    
+    return render_template('boxes.html', box = current_box)
+    
