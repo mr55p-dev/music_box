890cd43b145d9a827f846ae5ec28e8d3e5934c2b
Ellis <ellislunnon@gmail.com>
2020-08-16 15:55:16 +0100

    Currently writing decorator for auth required (box ownership).Need to research querying many to one related tables with SQLAlchemy. Changed up the file structure again also.

--- flask_server/routes/product.py
+++ flask_server/routes/product.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for
 from flask_login import current_user, login_required
-from flask_server.database.models import User, Box
 from flask_server import db
+from flask_server.database.models import User, Box, Song
+from flask_server.utils.decorators import requiresOwnership, trace
 
 product = Blueprint('product', __name__)
 
@@ -25,14 +26,40 @@ def registerBox():
     flash("Your box has been added.")
     return redirect(url_for('main.profile'))
 
-
 @product.route('/box/<boxID>')
 @login_required
 def boxView(boxID):
-    # Validate if the current user is the registered user of the box
-    current_box = Box.query.filter_by(boxID=boxID).first()
-    if current_box.userID is not current_user.id:
-        flash("This box is not registered to you, please check the link and try again.")
-        return redirect(url_for('main.profile'))
-    
+    # Big problem with this line here, some work needed on querying the relation - figure it out !!!
+    current_box = Box.owner.has(User.id==current_user.id)
     return render_template('boxes.html', box = current_box)
+
+@product.route('/box/<boxID>/addSong', methods=["POST"])
+@login_required
+def addSongToBox(boxID):
+    """
+    This is a tempoary method which is just meant to test
+    accessing one property through another in the database
+    models. Needs to be replaced with something that also 
+    communicates with the box and writes the song / checks
+    which songs are on the box.
+    """
+    return "AddSong"
+
+@product.route('/box/<boxID>/addSong')
+@login_required
+def boxSongsPage(boxID):
+    """
+    Again a tempoary method which just allows viewing the
+    songs in the database, it doesnt work properly as 
+    there are no boxes in real life and no clients to communicate
+    with. Just for testing.
+    """
+    all_songs = Song.query.all()
+    box_songs = Box.query.filter_by(boxID=boxID).first().songs
+    current_box = Box.query.filter_by(boxID=boxID).first()
+
+    return render_template('song-add.html', 
+        all_songs = all_songs,
+        box_songs = box_songs,
+        box = current_box
+    )
---
+++ flask_server/utils/decorators.py
@@ -0,0 +1,48 @@
+from functools import wraps
+from flask import current_app
+from flask_login import current_user
+from flask_server.database.models import User, Box
+
+def requiresOwnership(func):
+    """
+    Wrapper to specify that the userID must
+    own the boxID in question before a page 
+    can be accessed. Should be used with 
+    @login_rquired decorator as well as this
+    contains some extra checks (ie if the app
+    configuration is changed to not use 
+    authentication).
+
+    Can be used as a standard decorator.
+    Can only be used on functions which 
+    possess a boxID in the URL.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        current_box = Box.query.filter_by(boxID=kwargs["boxID"]).first()
+        if current_box.userID is not current_user.id:
+            return current_app.login_manager.unauthorized()
+        # print(f'Trace: function {func.__name__}\nWith args: {args}\nKeyword arguments: {kwargs}')
+        return func(*args, **kwargs)
+    return wrapper
+
+def trace(func):
+    """
+    Allows you to see the arguments and name of the decorated function
+    in realtime with a small wrapper
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'''
+        Trace: function {func.__name__}\n
+        With args: {args}\n
+        Keyword arguments: {kwargs}''')
+
+        ret = func(*args, **kwargs)
+
+        print(f'''
+            Trace: function {func.__name__}\n
+            Returned: {ret}
+            ''')
+        return func(*args, **kwargs)
+        
